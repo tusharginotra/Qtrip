d858f91f537e714a7c90b9954e774b937eee34cd
tusharginotra <77898206+tusharginotra@users.noreply.github.com>
2022-12-21 21:17:08 +0530

    removed unwanted comment

--- frontend/modules/reservation_page.js
+++ frontend/modules/reservation_page.js
@@ -25,14 +25,6 @@ function addReservationToTable(reservations) {
 
   //Conditionally render the no-reservation-banner and reservation-table-parent
 
-  /*
-    Iterating over reservations, adding it to table (into div with class "reservation-table") and link it correctly to respective adventure
-    The last column of the table should have a "Visit Adventure" button with id=<reservation-id>, class=reservation-visit-button and should link to respective adventure page
-
-    Note:
-    1. The date of adventure booking should appear in the format D/MM/YYYY (en-IN format) Example:  4/11/2020 denotes 4th November, 2020
-    2. The booking time should appear in a format like 4 November 2020, 9:32:31 pm
-  */
   if (reservations.length !== 0) {
     document.getElementById("no-reservation-banner").style.display = "none";
     document.getElementById("reservation-table-parent").style.display = "block";
